42749acf2c422765b2f3d62a8a770f5324951653
loma <57.loma@gmail.com>
2015-09-22 14:58:15 +0800

    update test for get index

--- test/controllers/places_controller_test.rb
+++ test/controllers/places_controller_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class PlacesControllerTest < ActionController::TestCase
-  test "the truth" do
-    assert true
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:places)
   end
 end
