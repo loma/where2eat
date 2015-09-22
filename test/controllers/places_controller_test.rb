7511d48597b63447c267d9aaf8c6b40e61e52fed
loma <57.loma@gmail.com>
2015-09-22 15:19:44 +0800

    add controller to show and create

--- test/controllers/places_controller_test.rb
+++ test/controllers/places_controller_test.rb
@@ -6,4 +6,26 @@ class PlacesControllerTest < ActionController::TestCase
     assert_response :success
     assert_not_nil assigns(:places)
   end
+
+  test "should create new place" do
+    get :new
+    assert_response :success
+    assert_not_nil assigns(:place)
+  end
+
+  test "should show place by id" do
+    get(:show, {'id' => 1})
+    assert_response :success
+    assert_not_nil assigns(:place)
+  end
+
+  test "create place" do
+    assert_difference('Place.count') do
+      post :create, place: {
+        name: 'Opposite Food Court',
+        location: '150 Cantonment Rd.'
+      }
+    end
+    assert_redirected_to place_path(assigns(:place))
+  end
 end
