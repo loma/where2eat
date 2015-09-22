6741512ee24e1c7af25b89c441ceb1c78ee19f6b
Stanly Lau <stanly@odd-e.com>
2015-09-22 14:34:56 +0800

    Adding new action 'pickforme' for #2

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -33,13 +33,18 @@ class PlacesController < ApplicationController
       else
         render 'new'
       end
-  end  
+  end
 
   def destroy
     @place = Place.find(params[:id])
     @place.destroy
 
     redirect_to places_path
+  end
+
+  def pickforme
+    @places = Place.all
+    render 'index'
   end
 
   private
