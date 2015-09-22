a30c3b93fa6257c145409f1caa43f20a9507859f
loma <57.loma@gmail.com>
2015-09-22 12:17:39 +0800

    add listing and update

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,10 +1,28 @@
 class PlacesController < ApplicationController
   def index
-    @places = Array.new
+    @places = Place.all
   end
 
   def new
     @place = Place.new
+  end
+
+  def show
+    @place = Place.find(params[:id])
+  end
+
+  def edit
+    @place = Place.find(params[:id])
+  end
+
+  def update
+    @place = Place.find(params[:id])
+
+    if @place.update(place_params)
+      redirect_to @place
+    else
+      render 'edit'
+    end
   end
 
   def create
